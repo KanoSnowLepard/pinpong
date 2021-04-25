7e2d870a16b23773d57c8d235ea2f420081d8866
KanoSnowLepard <82322908+KanoSnowLepard@users.noreply.github.com>
2021-04-25 17:19:12 +0300

    Update pong.py

--- pong.py
+++ pong.py
@@ -30,9 +30,14 @@ class Player(GameSprite):
 font.init()
 text = font.SysFont("Arial.ttf", 100).render("Im black",True,(0,0,0))
 
-player1 = Player("argo1.png", 10,200,100,150,10)
-player2= Player("argo2.png", 610,200,100,150,10)
-ball = GameSprite("catball.png",300,300,75,75,30)
+font1 = font.Font(None, 35)
+lose1 = font1.render("igrok 2 win",True,(0,0,0))
+font2 = font.Font(None, 35)
+lose2 = font2.render("igrok 1 win",True,(0,0,0))
+
+player1 = Player("argo1.png", 10,200,20,150,10)
+player2= Player("argo2.png", 640,200,20,150,10)
+ball = GameSprite("catball.png",300,300,50,50,30)
 
 win_width = 700
 win_height = 500
@@ -43,14 +48,30 @@ display.set_caption("bong")
 clock = time.Clock()
 game = True 
 
+speed_x = 3
+speed_y = 3
+
 while game:
     for e in event.get():
         if e.type == QUIT :
             game == False
+    
     window.fill((225,225,225))
     ball.reset()
-    player1.update_L()
-    player2.update_r()
+    ball.rect.y +=speed_y
+    ball.rect.x +=speed_x
+    if ball.rect.y > 450 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200,200))
+    if ball.rect.x > 650:
+        finish = True
+        window.blit(lose2, (200,200))
+    player1.update_r()
+    player2.update_L()
     player1.reset()
     player2.reset()
     window.blit(text,(225,225))
